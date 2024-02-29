588adb8e17301679118c30c20fe5d92b8a28f771
Tatiane Muller <tatiane.muller@tributario.com.br>
2024-02-29 08:34:37 -0300

    feat: created entitys

--- src/main/java/com/sistemapagamento/domain/user/User.java
+++ src/main/java/com/sistemapagamento/domain/user/User.java
@@ -1,10 +1,8 @@
 package com.sistemapagamento.domain.user;
 
+import com.sistemapagamento.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,6 +11,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class User {
     @Id
@@ -28,4 +27,14 @@ public class User {
     private BigDecimal balance;
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO userDTO){
+        this.firstName = userDTO.firstName();
+        this.lastName = userDTO.lastName();
+        this.document = userDTO.document();
+        this.email = userDTO.email();
+        this.userType = userDTO.userType();
+        this.password = userDTO.password();
+
+    }
 }
